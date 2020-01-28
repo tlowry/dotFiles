9524e6e413213e13fb04200fc15f332695187f37
tlowry <tlowry@localhost.localdomain>
2020-01-28 06:33:28 -0500

    relative location install

--- install.sh
+++ install.sh
@@ -3,13 +3,15 @@
 #curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 #    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+LOC=$(pwd)
+
 # Create files if absent then append linking line if absent
-create_and_append ". ~/dotFiles/bash/tlowry-common.bashrc" ~/.bashrc
-create_and_append ". ~/dotFiles/bash/tlowry-common.profile" ~/.bash_profile
-create_and_append ":so ~/dotFiles/vim/tlowry.vimrc" ~/.vimrc 
+create_and_append ". ${LOC}/bash/tlowry-common.bashrc" ~/.bashrc
+create_and_append ". ${LOC}/bash/tlowry-common.profile" ~/.bash_profile
+create_and_append ":so ${LOC}/vim/tlowry.vimrc" ~/.vimrc 
 
 create_and_append "set editing-mode vi" ~/.inputrc
 
 # create vim theming dirs and softlink
 mkdir -p ~/.vim/colors/
-ln -s ~/dotFiles/vim/colors/codedark.vim ~/.vim/colors/codedark.vim
+ln -s ${LOC}/vim/colors/codedark.vim ~/.vim/colors/codedark.vim
