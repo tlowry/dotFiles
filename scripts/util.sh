6bc1549f398cedc0590766acb5f7fad3fa8e5b10
tlowry <tlowryie@gmail.com>
2020-06-12 18:21:11 +0100

    posix + safety

--- scripts/util.sh
+++ scripts/util.sh
@@ -1,10 +1,12 @@
+#!/bin/sh
+
 #  Append text to the end of a file if not already present
 append_if_missing(){
- grep -qxF "$1" $2 || echo "$1" >> $2
+ grep -qxF "$1" "$2" || echo "$1" >> "$2"
 }
 
 # Create files only if not extant and append contents if not already present
 create_and_append(){
- touch -c $2
- append_if_missing "$1" $2
+ touch -c "$2"
+ append_if_missing "$1" "$2"
 }
