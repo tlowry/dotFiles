e3a49230e5d357af5845c2fbd542f2e11946d9ad
tlowry <tonylowryie@gmail.com>
2020-09-08 11:56:43 +0100

    rename scripts to bin for brevity and consistency1

--- scripts/tlowry_term.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-gnome-terminal \
-   --window --working-directory="$HOME"\
-   --tab --working-directory="$HOME" \
-   --tab --working-directory="$HOME" \
-   --tab --working-directory="$HOME" \
-   --tab --working-directory="$HOME" \
-   --tab --working-directory="/root" -e "su"  &
-exit
