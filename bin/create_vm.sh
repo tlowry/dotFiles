1b4bb43a5b72e71d141ee6a6e4f45174a1112cbd
tlowry <tonylowryie@gmail.com>
2020-09-08 11:57:10 +0100

    Few changes to support scripts->bin move

---
+++ bin/create_vm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+virt-install \
+--name cent8 \
+--description "Test VM with CentOS 8" \
+--ram=4096 \
+--vcpus=2 \
+--os-type=Linux \
+--os-variant=rhel8.1 \
+--disk path=/var/lib/libvirt/images/centos8.qcow2,bus=virtio,size=20 \
+--vnc \
+--location /run/media/stor/app/os/centos-8.1.1911-x86_64-dvd1.iso \
+--network bridge=br0  \
+--extra-args ro
