f09443d1ddb72e7b8d8d1751c2558bd3344f8485
tlowry <tonylowryie@gmail.com>
2020-09-08 11:34:07 +0100

    install/uninstall improvements

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+MAIN_CONFS=( 
+    ${DOT_LOC}/config/i3/config         ${DOT_LOC}/config/alacritty/alacritty.yml 
+    ${DOT_LOC}/config/mpv/mpv.conf      ${DOT_LOC}/config/wget/wgetrc 
+    ${DOT_LOC}/config/sxhkd/sxhkdrc     ${DOT_LOC}/config/tmux/tmux.conf
+    ${DOT_LOC}/config/polybar/config    ${DOT_LOC}/config/X11/Xresources
+    ${DOT_LOC}/config/cmus/rc           ${DOT_LOC}/config/cmus/main.theme
+    ${DOT_LOC}/config/conky/conky.conf  ${DOT_LOC}/config/qutebrowser/config.py
+    ${DOT_LOC}/config/lf/lfrc           ${DOT_LOC}/config/newsboat/config
+    ${DOT_LOC}/config/openbox/rc.xml    ${DOT_LOC}/config/openbox/menu.xml
+    ${DOT_LOC}/config/mpd/mpd.conf
+    ${DOT_LOC}/config/wal/colorschemes/dark/supertango.json
+    ${DOT_LOC}/config/wal/colorschemes/dark/grey.json
+)
+
 make_link () {
     dest_dir=`dirname $2` 2> /dev/null
     [ -f $dest_dir ] || mkdir -p $dest_dir
@@ -18,7 +32,7 @@ ul_scripts () {
     dots_bin="${DOT_LOC}/scripts"
     for file in $bin_dir/*
     do
-        readlink -f "$file" | grep -q "$dots_bin" && unlink "$file"
+        readlink -f "$file" | grep -q "$dots_bin" && [ -L "$file" ] && unlink "$file"
     done
 }
 
@@ -41,7 +55,7 @@ ul_apps () {
     dot_apps="${DOT_LOC}/local/share/applications"
     for file in $app_dir/*
     do
-        readlink -f "$file" | grep -q "$dot_apps" && unlink "$file"
+        [ -L "$file" ] && readlink -f "$file" | grep -q "$dot_apps" && echo "unlink "$file""
     done
 }
 
@@ -56,6 +70,18 @@ ln_apps () {
     done
 }
 
+ln_conf() {
+    dest=`echo $1 | sed 's/.*\/config\///g'`
+    dest=$XDG_CONFIG_HOME/$dest
+    make_link "$1" "$dest"
+}
+
+ul_conf() {
+    dest=`echo $1 | sed 's/.*\/config\///g'`
+    dest=$XDG_CONFIG_HOME/$dest
+    [ -L "$dest" ] && unlink "$dest"
+}
+
 # common install for all platforms
 install_base () {
     echo "install base"
@@ -66,36 +92,22 @@ install_base () {
     create_and_append ":so ${DOT_LOC}/config/vim/tlowry.vimrc" ~/.vimrc 
     create_and_append "\$include ${DOT_LOC}/config/input/inputrc" ~/.inputrc
     
+
     # soft link config to standard location
+    [ -f ~/.bash_profile ] || make_link ${DOT_LOC}/config/bash_profile ~/.bash_profile
     make_link ${DOT_LOC}/config/vim/colors/codedark.vim ~/.vim/colors/codedark.vim
     make_link ${DOT_LOC}/config/vim/colors/colors-wal.vim ~/.vim/colors/colors-wal.vim
-    [ -f ~/.bash_profile ] || make_link ${DOT_LOC}/config/bash_profile ~/.bash_profile
-    make_link ${DOT_LOC}/config/i3/config $XDG_CONFIG_HOME/i3/config
-    make_link ${DOT_LOC}/config/alacritty/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
-    make_link ${DOT_LOC}/config/mpv/mpv.conf $XDG_CONFIG_HOME/mpv/mpv.conf
-    make_link ${DOT_LOC}/config/wget/wgetrc $XDG_CONFIG_HOME/wget/wgetrc
-    make_link ${DOT_LOC}/config/sxhkd/sxhkdrc $XDG_CONFIG_HOME/sxhkd/sxhkdrc
-    make_link ${DOT_LOC}/config/tmux/tmux.conf $XDG_CONFIG_HOME/tmux/tmux.conf
-    make_link ${DOT_LOC}/config/polybar/config $XDG_CONFIG_HOME/polybar/config
-    make_link ${DOT_LOC}/config/X11/Xresources $XDG_CONFIG_HOME/Xresources
-    make_link ${DOT_LOC}/config/wal/colorschemes/dark/supertango.json $XDG_CONFIG_HOME/wal/colorschemes/dark/supertango.json
-    make_link ${DOT_LOC}/config/wal/colorschemes/dark/grey.json $XDG_CONFIG_HOME/wal/colorschemes/dark/grey.json
-
-    make_link ${DOT_LOC}/config/cmus/rc $XDG_CONFIG_HOME/cmus/rc
-    make_link ${DOT_LOC}/config/cmus/main.theme $XDG_CONFIG_HOME/cmus/main.theme
-    make_link ${DOT_LOC}/config/conky/conky.conf $XDG_CONFIG_HOME/conky/conky.conf
-    make_link ${DOT_LOC}/config/qutebrowser/config.py $XDG_CONFIG_HOME/qutebrowser/config.py
-    make_link ${DOT_LOC}/config/lf/lfrc $XDG_CONFIG_HOME/lf/lfrc
-    make_link ${DOT_LOC}/config/newsboat/config $XDG_CONFIG_HOME/newsboat/config
-    make_link ${DOT_LOC}/config/openbox/rc.xml $XDG_CONFIG_HOME/openbox/rc.xml
-    make_link ${DOT_LOC}/config/openbox/menu.xml $XDG_CONFIG_HOME/openbox/menu.xml
+    
+    for x in ${MAIN_CONFS[@]};do
+        ln_conf "$x"
+    done
     
     ln_scripts
     ln_apps
 
     distro=`uname -a | cut -d " " -f 2` ;[ $distro == "archlinux" ] && install_arch
 
-    [ "$1" == "home" ] && install_private
+    [ -z "$PERSONAL" ] || install_private
 }
 
 uninstall() {
@@ -104,6 +116,11 @@ uninstall() {
     del_lit_line ":so ${DOT_LOC}/config/vim/tlowry.vimrc" ~/.vimrc 
     del_lit_line "\$include ${DOT_LOC}/config/input/inputrc" ~/.inputrc
     del_lit_line "export DOT_LOC=$DOT_LOC" ~/.bashrc
+
+    for x in ${MAIN_CONFS[@]};do
+        ul_conf "$x"
+    done
+
     ul_scripts
     ul_apps
 }
@@ -127,7 +144,7 @@ install_arch() {
 	xdg-mime default sxiv.desktop image/png
 	xdg-mime default zathura.desktop application/epub+zip
 	xdg-mime default zathura.desktop application/pdf
-    	xdg-mime default vim.desktop text/plain
+    xdg-mime default vim.desktop text/plain
 
 	xdg-mime default calibre-ebook-viewer.desktop application/x-mobipocket-ebook
 	xdg-mime default calibre-ebook-viewer.desktop application/x-mobi8-ebook 
@@ -139,11 +156,13 @@ usage() {
 
 while [ "$1" != "" ]; do
     case $1 in
+        -p | --personal )       PERSONAL=1
+				;;
         -r | --remove )         REMOVE=1
-                                ;;
+				;;
         -h | --help )           usage
                                 exit
-                                ;;
+				;;
         * )                     ;; 
     esac
     shift
@@ -154,4 +173,5 @@ DOT_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . $DOT_LOC/scripts/util.sh
 
 [ -z $XDG_CONFIG_HOME ] && export XDG_CONFIG_HOME=~/.config 
-if [ -z "$REMOVE" ]; then install_base; else uninstall ; fi
+
+if [ -z "$REMOVE" ]; then install_base ; else uninstall ; fi
